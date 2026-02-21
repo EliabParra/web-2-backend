19984a1d2bea6644b070443393b81ae2cc177367
EliabParra <eliabparra@gmail.com>
2026-02-21 12:58:30 -0400

    chore(seed): remove duplicate legacy dml data files to ensure persistence consistency

--- migrations/dml/90_data_020_security_objects.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Auto-generated data for security.objects
- * Generated at: 2026-02-20T10:28:05.104Z
- */
-export const DATA_OBJECTS_SCHEMA = [
-    `INSERT INTO security.objects (object_id, object_name) VALUES ('1', 'Auth') ON CONFLICT (object_id) DO UPDATE SET object_name = EXCLUDED.object_name;`,
-    `INSERT INTO security.objects (object_id, object_name) VALUES ('3', 'Notification') ON CONFLICT (object_id) DO UPDATE SET object_name = EXCLUDED.object_name;`,
-]
--- migrations/dml/90_data_230_security_sessions.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Auto-generated data for security.sessions
- * Generated at: 2026-02-20T10:28:05.131Z
- */
-export const DATA_SESSIONS_SCHEMA = [
-    `INSERT INTO security.sessions (sid, sess, expire) VALUES ('EXoGxpqkb9YanZ9pkqPvjeHv5JeLaKAd', '{"cookie":{"originalMaxAge":1800000,"expires":"2026-02-20T01:36:03.802Z","secure":false,"httpOnly":true,"path":"/","sameSite":"lax"},"csrfToken":"15d565e4ea429e4aed8778f3db167c4ee496cdad0bf12a7a0da755d856e400dc","userId":"1","username":"admin","profileId":"1","email":"eliabparra@gmail.com"}', '2026-02-20T05:37:35.000Z') ON CONFLICT (sid) DO UPDATE SET sess = EXCLUDED.sess, expire = EXCLUDED.expire;`,
-]
