a35d7a19e5e75d18b151504c42d038f0e3ee7241
EliabParra <eliabparra@gmail.com>
2026-02-21 14:18:14 -0400

    fix(db): move 99_reset_sequences to dml and replace strict strings with pg_get_serial_sequence to allow idempotent bo Syncs

--- migrations/dml/99_reset_sequences.ts
+++ migrations/dml/99_reset_sequences.ts
@@ -5,34 +5,34 @@
  */
 export const RESET_SEQUENCES_SCHEMA = [
     // Reset transactions sequence
-    `SELECT setval('security.transactions_transaction_id_seq', 
+    `SELECT setval(pg_get_serial_sequence('security.transactions', 'transaction_id'), 
         COALESCE((SELECT MAX(transaction_id) FROM security.transactions), 0) + 1, false);`,
 
     // Reset methods sequence
-    `SELECT setval('security.methods_method_id_seq', 
+    `SELECT setval(pg_get_serial_sequence('security.methods', 'method_id'), 
         COALESCE((SELECT MAX(method_id) FROM security.methods), 0) + 1, false);`,
 
     // Reset objects sequence
-    `SELECT setval('security.objects_object_id_seq', 
+    `SELECT setval(pg_get_serial_sequence('security.objects', 'object_id'), 
         COALESCE((SELECT MAX(object_id) FROM security.objects), 0) + 1, false);`,
 
     // Reset profiles sequence
-    `SELECT setval('security.profiles_profile_id_seq', 
+    `SELECT setval(pg_get_serial_sequence('security.profiles', 'profile_id'), 
         COALESCE((SELECT MAX(profile_id) FROM security.profiles), 0) + 1, false);`,
 
     // Reset users sequence
-    `SELECT setval('security.users_user_id_seq', 
+    `SELECT setval(pg_get_serial_sequence('security.users', 'user_id'), 
         COALESCE((SELECT MAX(user_id) FROM security.users), 0) + 1, false);`,
 
     // Reset user_profile sequence
-    `SELECT setval('security.user_profile_user_profile_id_seq', 
+    `SELECT setval(pg_get_serial_sequence('security.user_profile', 'user_profile_id'), 
         COALESCE((SELECT MAX(user_profile_id) FROM security.user_profile), 0) + 1, false);`,
 
     // Reset profile_method sequence
-    `SELECT setval('security.profile_method_profile_method_id_seq', 
+    `SELECT setval(pg_get_serial_sequence('security.profile_method', 'profile_method_id'), 
         COALESCE((SELECT MAX(profile_method_id) FROM security.profile_method), 0) + 1, false);`,
 
     // Reset object_method sequence
-    `SELECT setval('security.object_method_object_method_id_seq', 
+    `SELECT setval(pg_get_serial_sequence('security.object_method', 'object_method_id'), 
         COALESCE((SELECT MAX(object_method_id) FROM security.object_method), 0) + 1, false);`,
 ]
