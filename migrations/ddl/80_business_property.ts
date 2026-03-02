d7a40cb2cb7fdb656f3d7d30fcf0c11fc4d63167
ErickMDR <erickdiazr27@gmail.com>
2026-03-02 19:55:53 -0400

    feat(db): property table

---
+++ migrations/ddl/80_business_property.ts
@@ -0,0 +1,15 @@
+/**
+ * Auto-generated schema for business.property
+ * Generated at: 2026-03-02T23:53:41.533Z
+ */
+export const PROPERTY_SCHEMA = [
+    // Table Definition
+    `create table if not exists business.property (
+        property_id integer not null default nextval('business.property_property_id_seq'::regclass),
+        property_description character varying not null,
+        property_value integer not null
+    );`,
+
+    // Indexes
+    `CREATE UNIQUE INDEX property_pkey ON business.property USING btree (property_id);`, 
+]
