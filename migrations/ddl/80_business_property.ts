33f590b466d37e6168dcba536f0ca865ea6b4bed
EliabParra <eliabparra@gmail.com>
2026-03-03 07:34:23 -0400

    refactor: define `property_id` as `serial primary key` in `business.property` table.

--- migrations/ddl/80_business_property.ts
+++ migrations/ddl/80_business_property.ts
@@ -5,11 +5,8 @@
 export const PROPERTY_SCHEMA = [
     // Table Definition
     `create table if not exists business.property (
-        property_id integer not null default nextval('business.property_property_id_seq'::regclass),
+        property_id serial primary key,
         property_description text not null,
         property_value integer not null
     );`,
-
-    // Indexes
-    `CREATE UNIQUE INDEX property_pkey ON business.property USING btree (property_id);`, 
 ]
