a79771540d2debc44c2b0c63976aacbdb2b4b7c0
EliabParra <eliabparra@gmail.com>
2026-03-17 23:50:52 -0400

    fix(dml): update category_type seed data

--- migrations/dml/90_data_999_business_category_type.ts
+++ migrations/dml/90_data_999_business_category_type.ts
@@ -1,9 +1,8 @@
 /**
  * Auto-generated data for business.category_type
- * Generated at: 2026-03-03T14:45:24.814Z
+ * Generated at: 2026-03-18T02:22:46.142Z
  */
 export const DATA_CATEGORY_TYPE_SCHEMA = [
-    // TODO(REVERT_NAMING): Revert category_type_de to category_type_description
     `INSERT INTO business.category_type (category_type_id, category_type_de) VALUES (1, 'Equipo') ON CONFLICT (category_type_id) DO UPDATE SET category_type_de = EXCLUDED.category_type_de;`,
     `INSERT INTO business.category_type (category_type_id, category_type_de) VALUES (2, 'Componente') ON CONFLICT (category_type_id) DO UPDATE SET category_type_de = EXCLUDED.category_type_de;`,
 ]
